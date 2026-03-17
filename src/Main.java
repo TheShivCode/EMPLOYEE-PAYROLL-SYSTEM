288edb2aa694743a71dc2451da77e15c3f43efc7
TheShivCode <shivpratap4581@gmail.com>
2026-03-17 19:03:20 +0530

    classes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,73 @@
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Employee{
+    private String name;
+    private int id;
+
+
+    public Employee(String name, int id){
+        this.name=name;
+        this.id=id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getId(){
+        return id;
+    }
+
+    public abstract double calculateSalary();
+
+
+    @Override
+    public String toString(){
+        return "Employee[name="+name+", id="+id+", salary= "+calculateSalary()+"]";
+    }
+class FullTimeEmployee extends Employee{
+        private double monthlySalary;
+
+        public FullTimeEmployee(String name, int id, double monthlySalary){
+            super(name,id);
+            this.monthlySalary= monthlySalary;
+        }
+
+        @Override
+        public double calculateSalary(){
+            return monthlySalary;
+        }
+}
+class PartTimeEmployee extends Employee{
+
+        private int hoursWorked;
+
+        private double hourlyRate;
+
+        public PartTimeEmployee(String name, int id, int hoursWorked, int hourlyRate){
+            super(name, id);
+            this.hoursWorked=hoursWorked;
+            this.hourlyRate=hourlyRate;
+        }
+
+        @Override
+        public double calculateSalary(){
+            return hoursWorked * hourlyRate;
+        }
+
+
+}
+
+class PayrollSystem{
+        private ArrayList<Employee> employeeList;
+
+        public PayrollSystem(){
+            employeeList = new ArrayList<>();
+        }
+}
+
+}
+
 public class Main {
     public static void main(String[] args) {
 
